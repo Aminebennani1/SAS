b0d456470f453afcf0f9e34969581e2016e519d1
Aminebennani1 <147489402+Aminebennani1@users.noreply.github.com>
2024-09-11 13:01:57 +0100

    Update tuto1.js

--- tuto1.js
+++ tuto1.js
@@ -1,21 +1,37 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('Quel est le titre ? ', (titre) => {
-  rl.question('Quelle est la durée ? ', (duree) => {
-    rl.question('Quelle est la date de début ? ', (dateDebut) => {
-      rl.question('Qui est le formateur ? ', (formateur) => {
-        console.log(`Détails de la formation :`);
-        console.log(`Titre : ${titre}`);
-        console.log(`Durée : ${duree}`);
-        console.log(`Date de début : ${dateDebut}`);
-        console.log(`Formateur : ${formateur}`);
-        rl.close();
-      });
-    });
-  });
-});
+const askQuestion = (query) => {
+  return new Promise((resolve) => rl.question(query, resolve));
+};
+
+const Add = (first_name, last_name) => {
+  console.log(first_name, last_name);
+};
+
+const Delete = (id) => {
+  console.log(`delete ${id}`);
+};
+
+const startCalculator = async () => {
+  let first_name;
+  let last_name;
+  let id = 0;
+  const letter = await askQuestion("Enter the letter (a, d): ");
+  if (letter == "a") {
+    first_name = await askQuestion("Enter the first_name: ");
+    last_name = await askQuestion("Enter the last_name: ");
+    Add(first_name, last_name);
+  } else if (letter == "d") {
+    id = await askQuestion("Enter the id: ");
+    Delete(id);
+  }
+
+  rl.close();
+};
+
+startCalculator();
